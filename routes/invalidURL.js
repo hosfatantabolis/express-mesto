ab1030f875fa10bdc505d6a5b77f6625b4cc0948
Maksim Litvinov <hosfatantaboliss@yandex.ru>
2021-02-03 01:21:31 +0300

    bugfix: mistakes correction 2

--- routes/invalidURL.js
+++ routes/invalidURL.js
@@ -1,5 +1,5 @@
 const invalidRouter = require('express').Router();
 const { INVALID_URL } = require('../utils/error_messages');
 
-invalidRouter.get('/', (req, res) => res.status(404).send(INVALID_URL));
+invalidRouter.use('/', (req, res) => res.status(404).send(INVALID_URL));
 module.exports = invalidRouter;
