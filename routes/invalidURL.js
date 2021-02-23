f2546345ee62ba923d40db7d13d6fba5e7d556a3
Maksim Litvinov <hosfatantaboliss@yandex.ru>
2021-02-23 04:19:30 +0300

    hotfix: message

--- routes/invalidURL.js
+++ routes/invalidURL.js
@@ -1,5 +1,5 @@
 const invalidRouter = require('express').Router();
 const { INVALID_URL } = require('../utils/error_messages');
 
-invalidRouter.use('/', (req, res) => res.status(404).send(INVALID_URL));
+invalidRouter.use('/', (req, res) => res.status(404).send({ message: INVALID_URL }));
 module.exports = invalidRouter;
